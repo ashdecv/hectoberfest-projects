acbe6c4b9c4ec0ffa5f1358f46c7ea5428611e72
ashdecv <91773528+ashdecv@users.noreply.github.com>
2021-10-02 12:23:29 +0530

    Update ascii of A.cpp
    please accplet

--- ascii of A.cpp
+++ ascii of A.cpp
@@ -15,5 +15,22 @@ int main()
     ch=ch+1;
     num=ch;
     cout << "The ASCII code for" << ch<<"is"<<num<<"\n"<<"\n";
-    return 0;
+    return 9;
+tost "i will meet 
+}
+int main()
+{
+    char ch;
+    int num=ch;
+    cout << "The ASCII code for" << ch<<"is"<<num<<"\n"<<"\n";
+    cout << "Adding 1 to the character code" <<"\n"<<"\n";
+    ch=ch+1;
+    num=ch;
+    cout << "The ASCII code for" << ch<<"is"<<num<<"\n"<<"\n";
+    cout << "Adding 1 to the character code" <<"\n"<<"\n";
+    ch=ch+1;
+    num=ch;
+    cout << "for asagg" << ch<<"is"<<num<<"\n"<<"\n";
+    return 9;
+tost "i will meet 
 }
